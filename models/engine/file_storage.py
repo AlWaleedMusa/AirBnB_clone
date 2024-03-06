6e7802658c89ec4379cd720a42a75c84ba24b092
AlWaleed Musa <jako_249@hotmail.com>
2024-03-07 01:23:37 +0200

    all tasks pushed #1

---
+++ models/engine/file_storage.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+
+import json
+import os
+
+class FileStorage:
+	""" make a file storage class """
+
+	__file_path = 'file.json'
+	__objects = {}
+
+
+	def all(self):
+		""" return all objects in the dict """
+
+		return self.__objects
+
+
+	def new(self, obj):
+		"""
+			make a new object ready with it class name and id as a key
+
+			Args:
+				obj: object passed to save in the dict
+		"""
+
+		pk = "{}.{}".format(obj.__class__.__name__, obj.id)
+		self.__objects[pk] = obj
+
+
+	def save(self):
+		""" serialize objects and save to a json file """
+
+		with open(self.__file_path, 'w') as file:
+			data = {key: value.to_dict() for key, value in self.__objects.items()}
+			json.dump(data, file, indent=4)
+
+
+	def reload(self):
+		""" check if json file exist if yes retrieve data and return the object """
+
+		if os.path.exists(self.__file_path):
+			with open(self.__file_path) as file:
+				obj_dict = json.load(file)
+				return obj_dict
+			
+	def classes(self):
+		from models.base_model import BaseModel
+		from models. user import User
+		from models.state import State
+		from models.city import City
+		from models.amenity import Amenity
+		from models.place import Place
+		from models.review import Review
+
+		classes_dict = {
+			'BaseModel': BaseModel,
+			'User': User,
+			'State': State,
+			'City': City,
+			'Amenity': Amenity,
+			'Place': Place,
+			'Review': Review,
+		}
+
+		return classes_dict
---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""Defines unittest for models/base_model.py.
+
+Unittest classes:
+    TestBaseModel
+"""
+import unittest
+from models.base_model import BaseModel
+import datetime
+
+class TestBaseModel(unittest.TestCase):
+
+    def test_initialization_with_args(self):
+        now = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+        base_model = BaseModel(id="123", created_at=now, updated_at=now)
+        self.assertEqual(base_model.id, "123")
+        self.assertEqual(base_model.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f"), now)
+        self.assertEqual(base_model.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f"), now)
+
+    def test_initialization_without_args(self):
+        now = datetime.datetime.now()
+        base_model = BaseModel()
+        id = base_model.id
+        self.assertNotEqual(base_model.id, "123")
+        self.assertEqual(base_model.id, id)
+        self.assertEqual(base_model.created_at, now)
+        self.assertEqual(base_model.updated_at, now)
+
+    def test_str_representation(self):
+        base_model = BaseModel(id="123")
+        self.assertEqual(str(base_model), "[BaseModel] (123) {'id': '123'}")
+
+    def test_save_method(self):
+        base_model = BaseModel()
+        initial_updated_at = base_model.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        base_model.save()
+        self.assertNotEqual(base_model.updated_at, initial_updated_at)
+
+    def test_to_dict_method(self):
+        now = datetime.datetime.now()
+        base_model = BaseModel(id="123", created_at=now, updated_at=now)
+        base_model_dict = base_model.to_dict()
+        self.assertEqual(base_model_dict['__class__'], 'BaseModel')
+        self.assertEqual(base_model_dict['id'], '123')
+        self.assertEqual(base_model_dict['created_at'], now.isoformat())
+        self.assertEqual(base_model_dict['updated_at'], now.isoformat())
+        
+	
+
+if __name__ == '__main__':
+    unittest.main()
