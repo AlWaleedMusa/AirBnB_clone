31ae02d0d69a12ffead9568917b808a14d88b046
AlWaleed Musa <jako_249@hotmail.com>
2024-03-07 02:23:58 +0200

    pycodestyle fixed in all other files #7

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,18 +3,17 @@
 import json
 import os
 
+
 class FileStorage:
     """ make a file storage class """
 
     __file_path = 'file.json'
     __objects = {}
 
-
     def all(self):
         """ return all objects in the dict """
 
         return self.__objects
-
 
     def new(self, obj):
         """
@@ -28,15 +27,14 @@ class FileStorage:
         pk = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[pk] = obj
 
-
     def save(self):
         """ serialize objects and save to a json file """
 
         with open(self.__file_path, 'w') as file:
-            data = {key: value.to_dict() for \
-                    key, value in self.__objects.items()}
+            data = {
+                key: value.to_dict() for key,
+                value in self.__objects.items()}
             json.dump(data, file, indent=4)
-
 
     def reload(self):
         """ check if json file exist if yes retrieve
@@ -46,7 +44,7 @@ class FileStorage:
             with open(self.__file_path) as file:
                 obj_dict = json.load(file)
                 return obj_dict
-            
+
     def classes(self):
         from models.base_model import BaseModel
         from models. user import User
